2db631a39f315eb8f2ba3e43b7a65d77938b5d25
ViCabral <vinicius.cabral@infojrunesp.com.br>
2020-04-15 21:46:53 -0300

    Included SearchBar Component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
+import {Feather} from '@expo/vector-icons';
+
+const SearchBar = ({ term, onTermChange }) => {
+
+    return (
+        <View style = {styles.background}>
+            <Feather name = "search" style = {styles.iconStyle}/>
+            <TextInput style = {styles.inputStyle}
+                autoCapitalize = "none"
+                autoCorrect = {false}
+                placeholder = "Search"
+                value = {term}
+                onChangeText = {newTerm => onTermChange(newTerm)}
+            />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#E8E8E8',
+        height: 50,
+        marginHorizontal: 40,
+        borderRadius: 10,
+        margin: 20,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    inputStyle: {
+        flex: 1,
+        marginHorizontal: 5,
+        fontSize: 16
+    },
+    iconStyle :{
+        fontSize: 30,
+        alignSelf: 'center',
+        marginHorizontal: 5
+    }
+});
+
+export default SearchBar;
